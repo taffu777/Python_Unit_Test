753d6c3b9fadcdc270e5320464924761b97caad7
Ansari Mohammed Tafazzul <125539287+taffu777@users.noreply.github.com>
2023-12-18 15:11:07 +0530

    6 Update test_atg_world.py

--- test_atg_world.py
+++ test_atg_world.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 
 class TestAtgWorld(unittest.TestCase):
     def setUp(self):
-        # ... your existing code ...
+        # Your setup code here
+        self.driver = webdriver.Chrome()
 
     def test_website_loading(self):
-        # ... your existing code ...
+        # Your test code here
+        self.driver.get("https://atg.world")
+        self.assertTrue("ATG" in self.driver.title)
 
     def tearDown(self):
-        # ... your existing code ...
+        # Your teardown code here
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
