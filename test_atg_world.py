bcb0b6f2f44d12cedd5546cb772a2b762697bde5
Ansari Mohammed Tafazzul <125539287+taffu777@users.noreply.github.com>
2023-12-18 15:17:18 +0530

    7 Update test_atg_world.py

--- test_atg_world.py
+++ test_atg_world.py
@@ -1,19 +1,11 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class TestAtgWorld(unittest.TestCase):
     def setUp(self):
-        # Your setup code here
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-    def test_website_loading(self):
-        # Your test code here
-        self.driver.get("https://atg.world")
-        self.assertTrue("ATG" in self.driver.title)
-
-    def tearDown(self):
-        # Your teardown code here
-        self.driver.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+    # ... rest of your test ...
